8efb44bbd30fc3df6fbe26547fcc7696fc3f057e
bbakjae <qkrwogus0724@naver.com>
2022-04-10 23:33:24 +0900

    window size & batch size  클래스 변수화

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
 import os
-
+from time import sleep
+from apscheduler.schedulers.background import BackgroundScheduler
 from prediction import Prediction
 
 
+"""
+ -- interval --
+1분:       minute1
+3분:       minute3
+5분:       minute5
+10분:      minute10
+30분:      minute30
+60분:      minute60
+240분:     minute240
+일봉:      day
+주봉:      week
+월봉:      month
+"""
 filename = os.path.join(".", "checkpoint", "checkpoint.ckpt")
 market = "KRW-ZIL"
 interval = "minute1"
 
+scheduler = BackgroundScheduler()
 prediction = Prediction(market=market, interval=interval, filename=filename)
-prediction.learning_data()
+
+scheduler.add_job(prediction.learning_data, "cron", hour=0)
+scheduler.start()
+
+while True:
+    df = prediction.get_live_dataframe()
+    pred = prediction.get_predict(df)
+    print(pred)
+    sleep(2)
--- prediction.py
+++ prediction.py
@@ -14,92 +14,120 @@ import numpy as np
 import pyupbit
 
 
-def create_model(window_size=5) -> Sequential:
-    # 1차원 feature map 생성
-    model = Sequential([
-        Conv1D(filters=32, kernel_size=5, padding="causal", activation="relu", input_shape=[window_size, 5]),
-        LSTM(16, activation="tanh"),
-        Dense(16, activation="relu"),
-        Dense(1)
-    ])
-    loss = Huber()
-    optimizer = adam.Adam(0.0005)
-    model.compile(loss=loss, optimizer=optimizer, metrics=["mse"])
-    return model
-
-
-def df_train_test_split(df: DataFrame):
-    x_train, x_test, y_train, y_test = train_test_split(df.drop(columns='close'), df['close'], test_size=0.1,
-                                                        random_state=0, shuffle=False)
-    return x_train, x_test, y_train, y_test
-
-
-def windowed_dataset(x, y, shuffle, window_size=5, batch_size=32):
-    # X값 window dataset 구성
-    ds_x = tf.data.Dataset.from_tensor_slices(x)
-    ds_x = ds_x.window(window_size, shift=1, stride=1, drop_remainder=True)
-    ds_x = ds_x.flat_map(lambda x: x.batch(window_size))
-    # y값 추가
-    ds_y = tf.data.Dataset.from_tensor_slices(y[window_size:])
-    ds = tf.data.Dataset.zip((ds_x, ds_y))
-    if shuffle:
-        ds = ds.shuffle(1000)
-    return ds.batch(batch_size).prefetch(1)
-
-
-class Prediction():
+class Prediction:
     scaler: MinMaxScaler
     model: Sequential
     market: str
     interval: str
     filename: str
+    window_size: int  # 예측에 활용할 과거 데이터 개수
+    batch_size: int  # 분할해서 학습시킬 정보 보통 32 or 64
 
-    def __init__(self, market, interval, filename):
+    def __init__(self, market, interval, filename, window_size=5, batch_size=32):
         self.market = market
         self.interval = interval
         self.filename = filename
-        self.model = create_model()
+        self.model = self.create_model()
         self.scaler = MinMaxScaler()
+        self.window_size = window_size
+        self.batch_size = batch_size
 
     def get_dataframe(self) -> DataFrame:
         df = pyupbit.get_ohlcv(ticker=self.market, interval=self.interval, count=10000)
         return df
 
+    def create_model(self) -> Sequential:
+        # 1차원 feature map 생성
+        model = Sequential(
+            [
+                Conv1D(
+                    filters=32,
+                    kernel_size=5,
+                    padding="causal",
+                    activation="relu",
+                    input_shape=[self.window_size, 5],
+                ),
+                LSTM(16, activation="tanh"),
+                Dense(16, activation="relu"),
+                Dense(1),
+            ]
+        )
+        loss = Huber()
+        optimizer = adam.Adam(0.0005)
+        model.compile(loss=loss, optimizer=optimizer, metrics=["mse"])
+        return model
+
+    def df_train_test_split(df: DataFrame):
+        x_train, x_test, y_train, y_test = train_test_split(
+            df.drop(columns="close"),
+            df["close"],
+            test_size=0.1,
+            random_state=0,
+            shuffle=False,
+        )
+        return x_train, x_test, y_train, y_test
+
+    def windowed_dataset(
+        self,
+        x,
+        y,
+        shuffle,
+    ):
+        # X값 window dataset 구성
+        ds_x = tf.data.Dataset.from_tensor_slices(x)
+        ds_x = ds_x.window(self.window_size, shift=1, stride=1, drop_remainder=True)
+        ds_x = ds_x.flat_map(lambda x: x.batch(self.window_size))
+        # y값 추가
+        ds_y = tf.data.Dataset.from_tensor_slices(y[self.window_size :])
+        ds = tf.data.Dataset.zip((ds_x, ds_y))
+        if shuffle:
+            ds = ds.shuffle(1000)
+        return ds.batch(self.batch_size).prefetch(1)
+
     def scale_dataframe(self, df: DataFrame) -> DataFrame:
-        scale_cols = ['open', 'high', 'low', 'close', 'volume', 'value']
+        scale_cols = ["open", "high", "low", "close", "volume", "value"]
         scaled = self.scaler.fit_transform(df[scale_cols])
         df = pd.DataFrame(scaled, columns=scale_cols)
         return df
 
     def get_learned_data(self, train_data, test_data):
-        early_stopping = EarlyStopping(monitor='val_loss', patience=35)
-        checkpoint = ModelCheckpoint(self.filename, save_weights_only=True, save_best_only=True, monitor='val_loss',
-                                     verbose=1)
-        history = self.model.fit(train_data, validation_data=test_data, epochs=1000,
-                                 callbacks=[checkpoint, early_stopping])
+        early_stopping = EarlyStopping(monitor="val_loss", patience=35)
+        checkpoint = ModelCheckpoint(
+            self.filename,
+            save_weights_only=True,
+            save_best_only=True,
+            monitor="val_loss",
+            verbose=1,
+        )
+        history = self.model.fit(
+            train_data,
+            validation_data=test_data,
+            epochs=1000,
+            callbacks=[checkpoint, early_stopping],
+        )
         return history
 
-    def show_history(self, history, test_data, y_test, window_size=5):
-        print("%.7f" % (float(min(history.history['val_loss']))))
+    def show_history(self, history, test_data, y_test):
+        print("%.7f" % (float(min(history.history["val_loss"]))))
         self.model.load_weights(self.filename)
         pred = self.model.predict(test_data)
-        actual = np.asarray(y_test)[window_size:]
+        actual = np.asarray(y_test)[self.window_size :]
         actual = np.reshape(actual, (len(actual), 1))
         plt.figure(figsize=(10, 10))
-        plt.plot(actual, label='actual')
-        plt.plot(pred, label='prediction')
+        plt.plot(actual, label="actual")
+        plt.plot(pred, label="prediction")
         plt.legend()
         plt.show()
 
     def get_live_dataframe(self) -> DataFrame:
         df = self.get_dataframe()
-        scaled_df, scaler = self.scale_dataframe(df=df)
+        scaled_df = self.scale_dataframe(df=df)
         live_x, live_y = scaled_df.drop(columns="close"), scaled_df["close"]
-        live_df = windowed_dataset(live_x, live_y, False)
+        live_df = self.windowed_dataset(live_x, live_y, False)
         return live_df
 
-    def get_predict(self, df: DataFrame, filename):
-        self.model.load_weights(filename)
+    def get_predict(self, df: DataFrame) -> float:
+        self.model.load_weights(self.filename)
         prediction = self.model.predict(df)
         empty_df = np.zeros(shape=(len(prediction), 6))
         empty_df[:, 0] = prediction[:, 0]
@@ -109,8 +137,8 @@ class Prediction():
     def learning_data(self):
         df = self.get_dataframe()
         df = self.scale_dataframe(df)
-        x_train, x_test, y_train, y_test = df_train_test_split(df)
-        train_data = windowed_dataset(x_train, y_train, True)
-        test_data = windowed_dataset(x_test, y_test, False)
+        x_train, x_test, y_train, y_test = self.df_train_test_split(df)
+        train_data = self.windowed_dataset(x_train, y_train, True)
+        test_data = self.windowed_dataset(x_test, y_test, False)
         history = self.get_learned_data(train_data=train_data, test_data=test_data)
         self.show_history(history=history, test_data=test_data, y_test=y_test)
